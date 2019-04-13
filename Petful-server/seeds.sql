adda6fbdb7c04ca4658b2036899243608f8d7362
Irem Sen <secilreel@yahoo.com>
2019-04-12 21:04:01 -0400

    changed the images of the pets

--- Petful-server/seeds.sql
+++ Petful-server/seeds.sql
@@ -2,16 +2,16 @@ INSERT INTO cats(imageURL, imageDescription, name, sex, age, breed, story)
 
 VALUES
 ('https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 'Orange bengal cat with black stripes lounging on concrete.', 'Fluffy', 'Female', 2, 'Bengal', 'Thrown on the street', '2019-04-12 16:04:19')
-('https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 'Gray cat sitting', 'Smoke', 'Male', 4, 'Siamese', 'Thrown on the street', '2019-04-13 16:04:19')
-('https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 'Black cat', 'Audrey', 'Female', 3, 'Maine Coon', 'Thrown on the street','2019-04-14 16:04:19')
-('https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 'White cat', 'Potter', 'Male', 1, 'Bengal', 'Thrown on the street', '2019-04-15 16:04:19')
-('https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 'Gray cat', 'Maizel', 'Female', 2, 'Maine Coon', 'Thrown on the street', '2019-04-16 16:04:19')
+('https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'Gray cat sitting', 'Smoke', 'Male', 4, 'Siamese', 'Thrown on the street', '2019-04-13 16:04:19')
+('https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_1280.jpg', 'Black cat', 'Audrey', 'Female', 3, 'Maine Coon', 'Thrown on the street','2019-04-14 16:04:19')
+('https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg', 'White cat', 'Potter', 'Male', 1, 'Bengal', 'Thrown on the street', '2019-04-15 16:04:19')
+('https://visualhunt.com/photos/2/portrait-of-cat-lying-on-floor.jpg?s=l', 'Gray cat', 'Maizel', 'Female', 2, 'Maine Coon', 'Thrown on the street', '2019-04-16 16:04:19')
 
 INSERT INTO dogs( imageURL, imageDescription, name, sex, age, breed, story)
 
 VALUES
 ('http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg', 'A smiling golden-brown golden retreiver listening to music.', 'Zeus', 'Male', 3, 'Golden Retriever', 'Owner Passed away', '2019-04-12 16:04:19')
-('http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg', 'Mix-breed', 'Alpha', 'Male', 3, 'Mixed breed', 'Owner Passed away', '2019-04-13 16:04:19')
-('http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg', 'Poodle', 'Hermione', 'Female', 2, 'Poodle', 'Owner Passed away', '2019-04-14 16:04:19')
-('http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg', 'German Shephard', 'Helmut', 'Male', 3, 'German Shepard', 'Owner Passed away', '2019-04-15 16:04:19')
-('http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg', 'Mix-breed', 'Dumbledore', 'Male', 4, 'Mix breed', 'Owner Passed away', '2019-04-16 16:04:19')
+('https://images.dog.ceo/breeds/vizsla/n02100583_3748.jpg', 'Mix-breed', 'Alpha', 'Male', 3, 'Mixed breed', 'Owner Passed away', '2019-04-13 16:04:19')
+('https://images.dog.ceo/breeds/pyrenees/n02111500_4687.jpg', 'Poodle', 'Hermione', 'Female', 2, 'Poodle', 'Owner Passed away', '2019-04-14 16:04:19')
+('https://images.dog.ceo/breeds/malamute/n02110063_8757.jpg', 'German Shephard', 'Helmut', 'Male', 3, 'German Shepard', 'Owner Passed away', '2019-04-15 16:04:19')
+('https://images.dog.ceo/breeds/groenendael/n02105056_143.jpg', 'Mix-breed', 'Dumbledore', 'Male', 4, 'Mix breed', 'Owner Passed away', '2019-04-16 16:04:19')
