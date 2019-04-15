7364dc1fc88b3bac3587195e7ef43357bb520891
Irem Sen <secilreel@yahoo.com>
2019-04-14 21:00:26 -0400

    deployment

--- src/server.js
+++ src/server.js
@@ -14,10 +14,15 @@ const db = knex({
 });
 
 app.set('db', db);
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(
   cors({
-    origin: 'https://payman-irem-petful-app.now.sh'
+    origin: CLIENT_ORIGIN
   })
 );
 module.exports = { app };
